938c4b8e2e1cf73efd49c34752b84a9c3f5855f0
YashXBansal <yashbansal414@gmail.com>
2025-05-07 21:08:08 +0530

    frontend setup along with router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router";
+import Home from "../pages/Home.jsx";
+import Signup from "../pages/Signup.jsx";
+import Login from "../pages/Login.jsx";
+import Notifications from "../pages/Notifications.jsx";
+import Call from "../pages/Call.jsx";
+import Chat from "../pages/Chat.jsx";
+import Onboarding from "../pages/Onboarding.jsx";
+
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div data-theme="night">
+      <button onClick={() => toast.success("Hello world")}>
+        create a toast
+      </button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/notification" element={<Notifications />} />
+        <Route path="/call" element={<Call />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
-export default App
+export default App;
