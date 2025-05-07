62206c551393cf8cab7c671b0afd345d7b440b8c
YashXBansal <yashbansal414@gmail.com>
2025-05-07 16:49:21 +0530

    getting stream token

---
+++ server/controllers/chat.controller.js
@@ -0,0 +1,10 @@
+import { generateStreamToken } from "../lib/stream.js";
+
+export async function getStreamToken(req, res) {
+  try {
+    const token = generateStreamToken(req.user.id);
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to generate token" });
+  }
+}
